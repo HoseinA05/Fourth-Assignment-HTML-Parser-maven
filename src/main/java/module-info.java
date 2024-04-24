0718b80b439b487b98d12f690f9cec5c4672d2f9
HoseinA05 <asf.hosein@gmail.com>
2024-04-24 14:54:47 +0330

    Revert "Initial commit and the base Objectives"
    This reverts commit 54aafa7790620c6d25ad84034b8b47c3d0ac2e1c.

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.fourthassignmenthtmlparser {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.jsoup;
-
-
-    opens org.example.fourthassignmenthtmlparser to javafx.fxml;
-    exports org.example.fourthassignmenthtmlparser;
-}
